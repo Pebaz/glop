ccbec68d51ea972c19c3ad42824ceef7ee196985
Pebaz <pebazium@gmail.com>
2021-10-09 16:58:44 -0700

    Saving before going deeper

--- asm/print_hello.asm
+++ asm/print_hello.asm
@@ -20,39 +20,53 @@ section '.text' code executable readable
 ;     MOV       R0, R0(+2,0)
 ;     RET
 
+; ORIGINAL WORKING MAIN FUNCTION
+; efi_main:
+;     MOVn   R1, @R0(EFI_MAIN_PARAMETERS.SystemTable)
+;     MOVn   R1, @R1(EFI_SYSTEM_TABLE.ConOut)
+;     MOVREL R2, string_hello
+;     PUSHn  R2
+;     PUSHn  R1
+;     CALLEX @R1(SIMPLE_TEXT_OUTPUT_INTERFACE.OutputString)
+;     MOV R0, R0(+2,0)
+;     JMP efi_main
+;     RET
+
+;; void print(EFI_SYSTEM_TABLE * SystemTable);
 print:
-    ; POP R1
-    MOVn R1, @R0(+1, 0)
-    MOVn R1, @R1(EFI_SYSTEM_TABLE.ConOut)
-
+    ; MOVn R1, @R0(+1, 0)
+    POP R1
+    MOVn R4, @R1(EFI_SYSTEM_TABLE.ConOut)
+    MOVn R3, @R4(SIMPLE_TEXT_OUTPUT_INTERFACE.OutputString)
     MOVREL R2, string_hello
+
+    ;; Push parameters onto native stack in reverse order
     PUSHn R2
-
-    PUSHn R1
-
-    CALLEX    @R1(SIMPLE_TEXT_OUTPUT_INTERFACE.OutputString)
-    MOV       R0, R0(+2, 0)
-
+    PUSHn R4
+    CALLEX @R3
+    MOV R0, R0(+2, 0)  ;; POP, POP
+    PUSH R1
     RET
+
 
 ;; ! WE ARE LOSING THE POINTER TO EFI_SYSTEM_TABLE!!!!!
 ;; EFI_STATUS EfiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE * SystemTable);
 ;; Before we modify the stack again, we need to preserve the EFI_SYSTEM_TABLE
+;; This gets passed in: EFI_MAIN_PARAMETERS
 efi_main:
-    ; MOVREL R5, string_hello
-    ; PUSH R5  ;; This instantly breaks the ConOut stuff in print()
-
-    ;; Push EFI_SYSTEM_TABLE onto the stack
     MOVn R1, @R0(EFI_MAIN_PARAMETERS.SystemTable)
-    PUSH R1
+    MOVn R1, @R0(EFI_MAIN_PARAMETERS.SystemTable)
 
+    ;; Using normal PUSH here since it is used by print()
+    PUSHn R1  ;; Keep track of EFI_SYSTEM_TABLE
+loop1:
+    ;; As long as the stack doesn't get modified, we can rely on @RO(SysTable)
+    PUSH @R0(+1, 0)  ;; Push the EFI_SYSTEM_TABLE pointer for use by print()
     CALL print
-    MOV R0, R0(+1, 0)
-    POP R1
+    POP R6  ;; Not using R6
+    JMP loop1
 
-    JMP efi_main
-    RET
+
 
 section '.data' data readable writeable
-    gST: dq ?
     string_hello: du "Hello World!", 0x0A, 0x00
