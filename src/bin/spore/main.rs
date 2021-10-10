b89ffcd487547930edcc6b18fa11b67273522ef4
Pebaz <pebazium@gmail.com>
2021-10-09 22:57:53 -0700

    Opcodes

--- src/bin/spore/main.rs
+++ src/bin/spore/main.rs
@@ -8,6 +8,106 @@ Bash. What if polyglot programs exposed same interface?
 
 use std::io::prelude::*;
 
+// Make Instruction trait
+// Make structs for each instruction that can parse themselves
+
+
+enum Opcodes
+{
+    ADD = 0x0C,
+    AND = 0x14,
+    ASHR = 0x19,
+    BREAK = 0x00,
+    CALL = 0x03,
+    CMPeq = 0x05,
+    CMPlte = 0x06,
+    CMPgte = 0x07,
+    CMPulte = 0x08,
+    CMPugte = 0x09,
+    CMPIeq = 0x2D,
+    CMPIlte = 0x2E,
+    CMPIgte = 0x2F,
+    CMPIulte = 0x30,
+    CMPIugte = 0x31,
+    DIV = 0x10,
+    DIVU = 0x11,
+    EXTNDB = 0x1A,
+    EXTNDD = 0x1C,
+    EXTNDW = 0x1B,
+    JMP = 0x01,
+    JMP8 = 0x02,
+    LOADSP = 0x29,
+    MOD = 0x12,
+    MODU = 0x13,
+    MOVbw = 0x1D,
+    MOVww = 0x1E,
+    MOVdw = 0x1F,
+    MOVqw = 0x20,
+    MOVbd = 0x21,
+    MOVwd = 0x22,
+    MOVdd = 0x23,
+    MOVqd = 0x24,
+    MOVqq = 0x28,
+    MOVI = 0x37,
+    MOVIn = 0x38,
+    MOVnw = 0x32,
+    MOVnd = 0x33,
+    MOVREL = 0x39,
+    MOVsnw = 0x25,
+    MOVsnd = 0x26,
+    MUL = 0x0E,
+    MULU = 0x0F,
+    NEG = 0x0B,
+    NOT = 0x0A,
+    OR = 0x15,
+    POP = 0x2C,
+    POPn = 0x36,
+    PUSH = 0x2B,
+    PUSHn = 0x35,
+    RET = 0x04,
+    SHL = 0x17,
+    SHR = 0x18,
+    STORESP = 0x2A,
+    SUB = 0x0D,
+    XOR = 0x16
+}
+
+impl Instruction
+{
+    fn disassemble(&self)
+    {
+    }
+}
+
+fn bits(byte: u8) -> [bool; 8]
+{
+    let mut bits = [false; 8];
+
+    for i in 0 .. 8
+    {
+        if byte & 2u8.pow(i) > 0
+        {
+            bits[i as usize] = true;
+        }
+    }
+
+    bits
+}
+
+fn bits_to_byte(bits: &[bool]) -> u8
+{
+    let mut byte = 0;
+
+    for (i, bit) in bits.iter().enumerate()
+    {
+        if *bit
+        {
+            byte += 2u8.pow((bits.len() - i) as u32);
+        }
+    }
+    byte
+}
+
 /// Reads in an EFI Bytecode file from STDIN and prints the disassembly.
 fn main()
 {
@@ -24,10 +124,21 @@ fn main()
         {
             instruction[i % 4] = byte;
 
-            if i % 4 == 0 && i > 0
+            let bits = bits(byte);
+            let instruction_type_bits = &bits[0 .. 5];
+
+            let instruction_type = bits_to_byte(instruction_type_bits);
+
+            if instruction_type > 0
             {
-                println!("{:?}", instruction);
+                println!("{:?}", instruction_type);
             }
+
+            // if i % 4 == 0 && i > 0
+            // {
+            //     println!("{:?}", instruction);
+            //     instruction = [0; 4];
+            // }
         }
 
         // TODO(pbz): Bytes can be left over in the instruction. Process them.
