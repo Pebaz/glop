302321d291b106875308fd5bb912a686c230924d
Pebaz <pebazium@gmail.com>
2021-11-06 09:21:14 -0700

    Writing bytes to file

--- src/bin/glop/main.rs
+++ src/bin/glop/main.rs
@@ -1,5 +1,30 @@
+use std::io::prelude::*;
+use std::fs::File;
+
+pub trait FileWriter
+{
+    fn write_bytes(&mut self, data: &[u8]);
+}
+
+impl FileWriter for File
+{
+    fn write_bytes(&mut self, data: &[u8])
+    {
+        let mut pos = 0;
+
+        while pos < data.len()
+        {
+            pos += self.write(&data[pos..]).unwrap();
+        }
+    }
+}
+
 /// Glop compiler executable
 fn main()
 {
     println!("Hello Glop Compiler!");
+    let data = b"Some Bytes";
+    let mut file = File::create("out.efi").unwrap();
+
+    file.write_bytes(data);
 }
