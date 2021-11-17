9d168936c08694c4475a6deee7d85e28f236727a
Pebaz <pebazium@gmail.com>
2021-11-16 17:07:32 -0800

    Modified instruction pointer!

--- asm/glop_program.asm
+++ asm/glop_program.asm
@@ -7,18 +7,8 @@ format peebc efi  ; PE executable format, EFI Byte Code
 
 entry efi_main
 
-;; This is for uninitialized global variables and is used in leu of malloc
-section 'RESERVED' data readable writeable
-    system_table: dq ?
-
-;; This is for initialized global variables
-section 'DATA' data readable writeable
-    string_hello_world: du "Hello World!", 0x0D, 0x0A, 0x00
-
-
 ;; This is for all the assembly for the whole program
 section 'CODE' code executable readable
-
 
 print:
     MOVREL    R1, system_table
@@ -44,6 +34,26 @@ print:
 ;     PUSH R6  ;; Put the return address back
 ;     RET
 
+;; Pushes a 64-bit value onto the stack
+;; STACKPUSH(u64)
+STACKPUSH:
+    POP64 R6  ;; Save return address!
+
+    ;; This can be commented out for the same effect
+    POP64 R1  ;; Arg0
+    PUSH64 R1  ;; Arg0
+
+    JMP R6  ;; Jump to return address!
+
+
+;; STACKPOP()
+STACKPOP:
+    POP64 R6  ;; Save return address!
+
+    POP64 R1
+
+    JMP R6  ;; Jump to return address!
+
 
 
 efi_main:
@@ -52,11 +62,50 @@ efi_main:
     MOVn      @R1, @R0(EFI_MAIN_PARAMETERS.SystemTable)
 
     MOVREL R1, string_hello_world
-    PUSH R1
+
+
+
+        ;; THIS WORKED!
+        STORESP R6, [IP]
+        PUSH R6
+        MOVREL R1, string_status
+        PUSH R1
+        CALL print
+        POP R1
+        POP R6
+        JMP32 R6
+
+
+
+
+
+
+    ; PUSH R1
+    ;; CALL STACKPUSH
+    PUSH64 R1
+    STORESP R6, [IP]
+    MOVi R5, 8
+    ADD R6, R5
+    MOVREL R5, STACKPUSH
+    JMP32 R5
+
     CALL print
     POP R1
 
     loop_forever:
+        MOVREL R1, string_status
+        PUSH R1
+        CALL print
+        POP R1
         JMP loop_forever
 
     RET
+
+;; This is for uninitialized global variables and is used in leu of malloc
+section 'RESERVED' data readable writeable
+    system_table: dq ?
+
+;; This is for initialized global variables
+section 'DATA' data readable writeable
+    string_hello_world: du "Hello World!", 0x0D, 0x0A, 0x00
+    string_status: du "HERE", 0x0D, 0x0A, 0x00
